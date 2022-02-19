d0015753fea78a1efb26c95417189e7d17f5a2af
Lilah-Favour <lfavour@uw.edu>
2022-02-18 18:34:59 -0800

    Change Chart 2 to include gender

--- source/chart-2.R
+++ source/chart-2.R
@@ -9,16 +9,34 @@ gamingStudy <- read.csv("data/GamingStudy_filtered.csv",
 
 # Takes gamingStudy and makes a data frame by grouping by the GAD_T score and
 # averaging the number of hours played by people with each GAD score.
-averaged <- gamingStudy %>%
-  select(Hours, GAD_T) %>%
+female <- gamingStudy %>%
+  select(Hours, GAD_T, Gender) %>%
+  filter(Gender == "Female") %>%
   group_by(GAD_T) %>%
-  summarize(avg_hours = mean(Hours))
+  summarize(avg_hours = mean(Hours)) %>%
+  mutate(Gender = "Female")
+
+male <- gamingStudy %>%
+  select(Hours, GAD_T, Gender) %>%
+  filter(Gender == "Male") %>%
+  group_by(GAD_T) %>%
+  summarize(avg_hours = mean(Hours)) %>%
+  mutate(Gender = "Male")
+
+other <- gamingStudy %>%
+  select(Hours, GAD_T, Gender) %>%
+  filter(Gender == "Other") %>%
+  group_by(GAD_T) %>%
+  summarize(avg_hours = mean(Hours)) %>%
+  mutate(Gender = "Other")
+
+all_genders <- full_join(female, male) %>%
+  full_join(other)
 
 # Plots a scatter plot of the GAD assessment scores by the average hours of
 # games played
-ggplot(averaged, aes(x = GAD_T, y = avg_hours)) +
+ggplot(all_genders, aes(x = GAD_T, y = avg_hours, color = Gender)) +
   geom_point() +
-  geom_smooth(method=lm) +
+  geom_smooth(method = lm, se = FALSE) +
   labs(title = "Relationship Between Anxiety Levels and Hours of Gaming",
        x = "GAD Assessment Score", y = "Average Hours of Gaming Per Week")
-
