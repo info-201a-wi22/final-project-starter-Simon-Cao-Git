0e2317fa196070076c8a22e52f1c2fccd82c78f5
Lilah-Favour <lfavour@uw.edu>
2022-03-08 17:31:39 -0800

    added full working app code to tab_panel_chart1

--- Final Template/tabs/tab_panel_chart1.r
+++ Final Template/tabs/tab_panel_chart1.r
@@ -8,11 +8,11 @@ library("dplyr")
 gamingStudy <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-Simon-Cao-Git/main/data/GamingStudy_filtered.csv",
                         stringsAsFactors = FALSE)
 
+# defines ui
 select_test <- selectInput(
   inputId = "test",
   label = "Mental Health Test",
-  choices = list("Generalized Anxiety" = "GAD_T", "Satisfaction With Life" = "SWL_T",
-                 "Social Phobia" = "SPIN_T"),
+  choices = list("Generalized Anxiety", "Satisfaction With Life", "Social Phobia"),
   selected = "Generalized Anxiety"
 )
 
@@ -35,43 +35,129 @@ ui <- fluidPage(
   tab_panel_chart1
 )
 
+#defines server
 server <- function(input, output) {
+  
   output$scatterplot <- renderPlot({
     
-    # Takes gamingStudy and makes 3 data frames, 1 for each gender option. The data
-    # frames are made by grouping by the GAD_T score and averaging the number of 
-    # hours played by people with each GAD score.
-    female <- gamingStudy %>%
-      select(Hours, input$test, Gender) %>%
-      filter(Gender == "Female") %>%
-      group_by(input$test) %>%
-      summarize(avg_hours = mean(Hours)) %>%
-      mutate(Gender = "Female")
-    
-    male <- gamingStudy %>%
-      select(Hours, input$test, Gender) %>%
-      filter(Gender == "Male") %>%
-      group_by(input$test) %>%
-      summarize(avg_hours = mean(Hours)) %>%
-      mutate(Gender = "Male")
-    
-    other <- gamingStudy %>%
-      select(Hours, input$test, Gender) %>%
-      filter(Gender == "Other") %>%
-      group_by(input$test) %>%
-      summarize(avg_hours = mean(Hours)) %>%
-      mutate(Gender = "Other")
-    
-    # Full joins the three data frames separated by gender into one data frame
-    all_genders <- full_join(female, male) %>%
-      full_join(other)
-    
-    # Plots scatterplot of chosen test vs gaming hours
-    scatterplot <- ggplot(all_genders, aes(x = all_genders[input$test], y = avg_hours, color = Gender)) +
-      geom_point(na.rm = TRUE) +
-      geom_smooth(formula = y ~ x, method = lm, se = FALSE, na.rm = TRUE) +
-      labs(title = "Relationship Between Anxiety Levels and Hours of Gaming",
-           x = "Assessment Score", y = "Average Hours of Gaming Per Week")
-    return(scatterplot)
+    if (input$test == "Generalized Anxiety") {
+      
+      # Takes gamingStudy and makes 3 data frames, 1 for each gender option. The data
+      # frames are made by grouping by the GAD_T score and averaging the number of 
+      # hours played by people with each GAD score. 
+      female <- gamingStudy %>%
+        select(Hours, GAD_T, Gender) %>%
+        filter(Gender == "Female") %>%
+        group_by(GAD_T) %>%
+        summarize(avg_hours = mean(Hours)) %>%
+        mutate(Gender = "Female")
+      
+      male <- gamingStudy %>%
+        select(Hours, GAD_T, Gender) %>%
+        filter(Gender == "Male") %>%
+        group_by(GAD_T) %>%
+        summarize(avg_hours = mean(Hours)) %>%
+        mutate(Gender = "Male")
+      
+      other <- gamingStudy %>%
+        select(Hours, GAD_T, Gender) %>%
+        filter(Gender == "Other") %>%
+        group_by(GAD_T) %>%
+        summarize(avg_hours = mean(Hours)) %>%
+        mutate(Gender = "Other")
+      
+      # Full joins the three data frames separated by gender into one data frame
+      all_genders <- full_join(female, male) %>%
+        full_join(other)
+      
+      # Plots scatterplot of GAD test results vs gaming hours
+      scatterplot <- ggplot(all_genders, aes(x = GAD_T, y = avg_hours, color = Gender)) +
+        geom_point(na.rm = TRUE) +
+        geom_smooth(formula = y ~ x, method = lm, se = FALSE, na.rm = TRUE) +
+        labs(title = "Relationship Between Anxiety Levels and Hours of Gaming",
+             x = "Assessment Score", y = "Average Hours of Gaming Per Week")
+      
+      return(scatterplot)
+      
+    } else if (input$test == "Satisfaction With Life") {
+      
+      # Takes gamingStudy and makes 3 data frames, 1 for each gender option. The data
+      # frames are made by grouping by the GAD_T score and averaging the number of 
+      # hours played by people with each GAD score. 
+      female <- gamingStudy %>%
+        select(Hours, SWL_T, Gender) %>%
+        filter(Gender == "Female") %>%
+        group_by(SWL_T) %>%
+        summarize(avg_hours = mean(Hours)) %>%
+        mutate(Gender = "Female")
+      
+      male <- gamingStudy %>%
+        select(Hours, SWL_T, Gender) %>%
+        filter(Gender == "Male") %>%
+        group_by(SWL_T) %>%
+        summarize(avg_hours = mean(Hours)) %>%
+        mutate(Gender = "Male")
+      
+      other <- gamingStudy %>%
+        select(Hours, SWL_T, Gender) %>%
+        filter(Gender == "Other") %>%
+        group_by(SWL_T) %>%
+        summarize(avg_hours = mean(Hours)) %>%
+        mutate(Gender = "Other")
+      
+      # Full joins the three data frames separated by gender into one data frame
+      all_genders <- full_join(female, male) %>%
+        full_join(other)
+      
+      # Plots scatterplot of SWL test results vs gaming hours
+      scatterplot <- ggplot(all_genders, aes(x = SWL_T, y = avg_hours, color = Gender)) +
+        geom_point(na.rm = TRUE) +
+        geom_smooth(formula = y ~ x, method = lm, se = FALSE, na.rm = TRUE) +
+        labs(title = "Relationship Between Anxiety Levels and Hours of Gaming",
+             x = "Assessment Score", y = "Average Hours of Gaming Per Week")
+      
+      return(scatterplot)
+      
+    } else {
+      
+      # Takes gamingStudy and makes 3 data frames, 1 for each gender option. The data
+      # frames are made by grouping by the GAD_T score and averaging the number of 
+      # hours played by people with each GAD score.
+      female <- gamingStudy %>%
+        select(Hours, SPIN_T, Gender) %>%
+        filter(Gender == "Female") %>%
+        group_by(SPIN_T) %>%
+        summarize(avg_hours = mean(Hours)) %>%
+        mutate(Gender = "Female")
+      
+      male <- gamingStudy %>%
+        select(Hours, SPIN_T, Gender) %>%
+        filter(Gender == "Male") %>%
+        group_by(SPIN_T) %>%
+        summarize(avg_hours = mean(Hours)) %>%
+        mutate(Gender = "Male")
+      
+      other <- gamingStudy %>%
+        select(Hours, SPIN_T, Gender) %>%
+        filter(Gender == "Other") %>%
+        group_by(SPIN_T) %>%
+        summarize(avg_hours = mean(Hours)) %>%
+        mutate(Gender = "Other")
+      
+      # Full joins the three data frames separated by gender into one data frame
+      all_genders <- full_join(female, male) %>%
+        full_join(other)
+      
+      # Plots scatterplot of SPIN test results vs gaming hours
+      scatterplot <- ggplot(all_genders, aes(x = SPIN_T, y = avg_hours, color = Gender)) +
+        geom_point(na.rm = TRUE) +
+        geom_smooth(formula = y ~ x, method = lm, se = FALSE, na.rm = TRUE) +
+        labs(title = "Relationship Between Anxiety Levels and Hours of Gaming",
+             x = "Assessment Score", y = "Average Hours of Gaming Per Week")
+      
+      return(scatterplot)
+    }
   })
 }
+
+shinyApp(ui = ui, server = server)
